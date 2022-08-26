2b6429470e2f05266b29e490de2ae29b109fa4ad
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2022-08-26 15:23:39 +0530

    created getters and setters for the Employee type

--- src/main/java/org/example/employee/Employee.java
+++ src/main/java/org/example/employee/Employee.java
@@ -24,4 +24,44 @@ public class Employee {
         this.address = address;
         this.salary = salary;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
