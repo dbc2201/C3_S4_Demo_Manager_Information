61c3ba00b1399150f239a08b08b1cc726f5d0fb5
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2022-08-26 15:28:24 +0530

    created constructors for the RegionalHead type

--- src/main/java/org/example/employee/RegionalHead.java
+++ src/main/java/org/example/employee/RegionalHead.java
@@ -5,8 +5,20 @@
  */
 package org.example.employee;
 
+import java.time.LocalDate;
+
 public class RegionalHead extends Employee {
     private String regionName;
     private String regionPerformance;
     private int reviewRatings;
+
+    public RegionalHead() {
+    }
+
+    public RegionalHead(String name, int age, LocalDate dateOfBirth, String address, double salary, String regionName, String regionPerformance, int reviewRatings) {
+        super(name, age, dateOfBirth, address, salary);
+        this.regionName = regionName;
+        this.regionPerformance = regionPerformance;
+        this.reviewRatings = reviewRatings;
+    }
 }
