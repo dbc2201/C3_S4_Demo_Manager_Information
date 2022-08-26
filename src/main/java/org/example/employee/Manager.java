c39f2ebaee5e2a2eaa323ee67331f93d64a82a93
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2022-08-26 15:26:07 +0530

    created the constructors for the Manager class

--- src/main/java/org/example/employee/Manager.java
+++ src/main/java/org/example/employee/Manager.java
@@ -5,8 +5,26 @@
  */
 package org.example.employee;
 
+import java.time.LocalDate;
+
 public class Manager extends Employee {
     private int teamSize;
     private int rating;
     private double yearlyIncrement;
+
+    public Manager() {
+    }
+
+    public Manager(int teamSize, int rating, double yearlyIncrement) {
+        this.teamSize = teamSize;
+        this.rating = rating;
+        this.yearlyIncrement = yearlyIncrement;
+    }
+
+    public Manager(String name, int age, LocalDate dateOfBirth, String address, double salary, int teamSize, int rating, double yearlyIncrement) {
+        super(name, age, dateOfBirth, address, salary);
+        this.teamSize = teamSize;
+        this.rating = rating;
+        this.yearlyIncrement = yearlyIncrement;
+    }
 }
