60eb59e2f12722ea42dee4a2cb75f8779a079a21
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2022-08-26 15:40:56 +0530

    created objects for the Employee, Manager, and RegionalManager; migrate the type of regionPerformance to int

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,45 @@
 package org.example;
 
+import org.example.employee.Employee;
+import org.example.employee.Manager;
+import org.example.employee.RegionalHead;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = "Divyansh Bhardwaj";
+        int age = 26;
+        LocalDate dateOfBirth = LocalDate.of(2022, 8, 26);
+        String address = "Pune";
+        double salary = 100000.0;
+        Employee employee = new Employee(
+                name,
+                age,
+                dateOfBirth,
+                address,
+                salary
+        );
+        System.out.println(employee.getName());
+        Manager manager = new Manager(
+                name,
+                age,
+                dateOfBirth,
+                address,
+                salary,
+                10, 8, 12.5
+        );
+        System.out.println("manager = " + manager.getName());
+        RegionalHead regionalHead = new RegionalHead(
+                name,
+                age,
+                dateOfBirth,
+                address,
+                salary,
+                "Pune",
+                10,
+                9
+        );
+        System.out.println("regionalHead = " + regionalHead.getName());
     }
 }
