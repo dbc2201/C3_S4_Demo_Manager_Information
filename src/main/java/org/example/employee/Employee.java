103be116c6cd0154ed2df08185b7527ff378ab8a
Divyansh Bhardwaj <divyansh.bhardwaj@niit.com>
2022-08-26 15:23:20 +0530

    created constructors for the Employee type

--- src/main/java/org/example/employee/Employee.java
+++ src/main/java/org/example/employee/Employee.java
@@ -13,4 +13,15 @@ public class Employee {
     private LocalDate dateOfBirth;
     private String address;
     private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, int age, LocalDate dateOfBirth, String address, double salary) {
+        this.name = name;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.salary = salary;
+    }
 }
